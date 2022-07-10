7d35d64303c4a0bec64edacc46857ba4485dcc17
meduduna Akerejola <cremmybanj7@gmail.com>
2022-07-10 21:57:11 +0100

    ...

--- app/assets/components/Header.tsx
+++ app/assets/components/Header.tsx
@@ -1,12 +1,30 @@
 import React, { JSXElementConstructor } from 'react';
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Header:JSXElementConstructor<{}>=()=>{
     return (
-        <View>
-
+        <View style={styles.container}>
+            <View>
+                {/* Image of profile pic */}
+                <Text style={styles.chatText}>Chats</Text>
+            </View>
+            <View>
+                {/* Image of camera icon */}
+                {/* image of edit icon */}
+            </View>
         </View>
     );
 }
-
+const styles = StyleSheet.create({
+    container:{
+        marginTop:30,
+        flex:1,
+        flexDirection:'row',
+        height:50,
+        justifyContent:'space-between'
+    },
+    chatText:{
+        fontSize:50
+    }
+})
 export default Header;
